8aced4a331a552456251d3231a0424afa53af23a
mf-s <74371228+mf-s@users.noreply.github.com>
2021-06-21 23:00:06 +0300

    v1.0.0

--- bot.js
+++ bot.js
@@ -1,23 +1,32 @@
 const SteamUser = require('steam-user');
 const config = require('./config.json');
 const client = new SteamUser();
- 
 const logOnOptions = {
     accountName: config.username,
     password: config.password,
-
 };
+const replyEnabled = false;
+const repliededUsers = [];
 
 client.logOn(logOnOptions);
-
 client.on('loggedOn', () => {
-    console.log('succesfully logged on.');
+    console.log('Succesfully logged on.');
     client.setPersona(SteamUser.Steam.EPersonaState.Online);
     client.gamesPlayed(config.idlegameid);
-
 });
 
 client.on("friendMessage", function (steamID, message) {
-    client.chatMessage(steamID, config.afkmessage);
-
+    if(message == config.enableCommand){
+        console.log('Reply enabled');
+        replyEnabled = true;
+    } else if(message == config.disabledCommand) {
+        console.log('Reply disabled');
+        replyEnabled = false;
+    } else if(replyEnabled && !repliededUsers.includes(steamID)){
+        client.chatMessage(steamID, config.afkmessage);
+        repliededUsers.push(steamID);
+        setTimeout(() => {
+            repliededUsers = repliededUsers.filter(function(steamIDs) { return steamIDs !== steamID })
+        }, 10 * 60000); //10 min
+    }
 });
